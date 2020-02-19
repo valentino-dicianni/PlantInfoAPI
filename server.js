e091020a4fcf70ab26bd24647d1da77e5fa2a5a6
st148705 <valentino.dicianni@edu.unito.it>
2020-02-19 12:51:09 +0100

    setup

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express'),
     bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://test:test123@ds053648.mlab.com:53648/heroku_chms7xdj');
+mongoose.connect('mongodb://test:test123@ds053648.mlab.com:53648/heroku_chms7xdj', { useUnifiedTopology: true, useNewUrlParser: true });
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
