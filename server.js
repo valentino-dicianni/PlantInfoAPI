bd79373d619e1884a4b7d2cbd9c38a9150eb07be
st148705 <valentino.dicianni@edu.unito.it>
2020-02-21 11:54:56 +0100

    setup

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+var cors = require('cors')
+
 
 var express = require('express'),
     app = express(),
@@ -17,11 +19,9 @@ mongoose.connect('mongodb://test:test123@ds053648.mlab.com:53648/heroku_chms7xdj
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
+app.use(cors())
+
 
 var routes = require('./api/routes/plantRoutes');
 routes(app);
