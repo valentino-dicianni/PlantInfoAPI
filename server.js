f560392cb658ae886d33d4002f6b11ddc63bbd14
st148705 <valentino.dicianni@edu.unito.it>
2020-02-21 11:32:26 +0100

    setup

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ mongoose.connect('mongodb://test:test123@ds053648.mlab.com:53648/heroku_chms7xdj
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var routes = require('./api/routes/plantRoutes');
 routes(app);
